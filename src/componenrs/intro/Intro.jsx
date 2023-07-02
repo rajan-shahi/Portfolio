81f9df5653533857349883fd3ef6e621760ea857
rajan <rajanshahi1267@gmail.com>
2023-07-02 07:02:18 -0700

    add button

--- src/componenrs/intro/Intro.jsx
+++ src/componenrs/intro/Intro.jsx
@@ -1,43 +1,44 @@
 import "./intro.scss";
-import { init } from 'ityped'
-import  { useEffect,useRef } from "react"
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-
-
-export default function Intro()
- {
-
+export default function Intro() {
   const textRef = useRef();
 
   useEffect(() => {
-    init(textRef.current, { 
+    init(textRef.current, {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-
-      strings: ["Developer", "Designer","Content Creator" ],});
-
-  },[]);
-  return <div className="intro">
-    
-    <div className="left">
-    <div className="imageContainer">
-      <img src="assets/image/man.jpg" alt=" "></img>
-    </div>
-    </div>
-
-    <div className="right">
-      <div className="wrapper">
-        <h2>Hi There, I'm </h2>
-        <h1>Rajan Bahadur Shahi</h1>
-        <h3>Freelance <span ref={textRef}></span> </h3>
-
-        <a href="Portfolio" >
-        < img src="assets/image/down.png" alt=" "></img>
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+  return (
+    <div className="intro">
+      <a target="blank" href="http://www.google.com">
+        someth
       </a>
+      <a href="http://www.google.com">someth</a>
+
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/image/man.jpg" alt=" "></img>
+        </div>
       </div>
 
-    </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm </h2>
+          <h1>Rajan Bahadur Shahi</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>{" "}
+          </h3>
 
-  </div>;
+          <a href="Portfolio">
+            <img src="assets/image/down.png" alt=" "></img>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
